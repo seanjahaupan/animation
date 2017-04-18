9d442e9ab790582dcb4927cb685e7ec8b754c37b
seanjahaupan <seanjahaupan@gmail.com>
2017-04-17 18:43:16 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+import Expo from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Deck from './src/Deck';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Deck />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+});
+
+Expo.registerRootComponent(App);
